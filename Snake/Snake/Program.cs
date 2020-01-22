0888467f6148ed78ad4bca48fcd81302c9ecb1ab
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2020-01-22 21:56:57 +0300

    Конец игры

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -55,7 +55,26 @@ namespace Snake
                     snake.HandleKey(key.Key);
                 }
             }
+            WriteGameOver();
             Console.ReadKey();
+        }
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Автор: Андрей Гриценко", xOffset + 2, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
